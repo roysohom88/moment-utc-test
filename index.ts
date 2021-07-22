4987b6f81932fe02d9777100c94fb3ab6090cda2
roysohom88 <roy.sohom88@yahoo.in>
2021-07-22 11:16:51 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -1,17 +1,48 @@
 import moment from 'moment';
 console.clear();
+console.log('Original Purchase Date UTC');
+console.log(
+  `orininal_purchase_date_ms ==>> `,
+  moment(1607975423000)
+    .utc()
+    .format('YYYY-MM-DD HH:mm:ss')
+);
 console.log('Monthly');
-
-console.log('Half Yearly');
 console.log(
   `purchase_date_ms ==>> `,
-  moment(1623309540000)
+  moment(1623392072000)
     .utc()
     .format('YYYY-MM-DD HH:mm:ss')
 );
 console.log(
   `expires_date_ms ==>> `,
-  moment(1623311340000)
+  moment(1623392372000)
+    .utc()
+    .format('YYYY-MM-DD HH:mm:ss')
+);
+console.log('Monthly');
+console.log(
+  `purchase_date_ms ==>> `,
+  moment(1623392372000)
+    .utc()
+    .format('YYYY-MM-DD HH:mm:ss')
+);
+console.log(
+  `expires_date_ms ==>> `,
+  moment(1623392672000)
+    .utc()
+    .format('YYYY-MM-DD HH:mm:ss')
+);
+console.log('Half Yearly');
+console.log(
+  `purchase_date_ms ==>> `,
+  moment(1623418919000)
+    .utc()
+    .format('YYYY-MM-DD HH:mm:ss')
+);
+console.log(
+  `expires_date_ms ==>> `,
+  moment(1623420719000)
     .utc()
     .format('YYYY-MM-DD HH:mm:ss')
 );
@@ -19,13 +50,13 @@ console.log(
 console.log('Yearly');
 console.log(
   `purchase_date_ms ==>> `,
-  moment(1623309990000)
+  moment(1623392795000)
     .utc()
     .format('YYYY-MM-DD HH:mm:ss')
 );
 console.log(
   `expires_date_ms ==>> `,
-  moment(1623313590000)
+  moment(1623396395000)
     .utc()
     .format('YYYY-MM-DD HH:mm:ss')
 );
